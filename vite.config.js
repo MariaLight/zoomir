9a2a605a0bf8e6508d586b29e1377872f9c99a3c
MariaLight <61028743+MariaLight@users.noreply.github.com>
2025-09-22 17:22:26 +1200

    Билд

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'src/index.html'),
+        catalog: resolve(__dirname, 'src/catalog.html'),
       },
       output: {
         entryFileNames: 'js/[name]-[hash].js',
