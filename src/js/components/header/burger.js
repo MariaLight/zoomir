421776027a6370fba486c8efbd876cbf1f186d07
MariaLight <61028743+MariaLight@users.noreply.github.com>
2025-09-24 13:50:34 +1200

    Добавила бургер-меню на планшетах

--- src/js/components/header/burger.js
+++ src/js/components/header/burger.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
 
     const burgerBtn = document.querySelector('.burger-btn');
+    const burgerMenu = document.querySelector('.header__burger-menu');
 
     burgerBtn.addEventListener('click', () => {
         burgerBtn.classList.toggle('active');
+        burgerMenu.classList.toggle('active');
     })
     
     
